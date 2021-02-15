8fdaebc18ad1e098efcd36dfe39b7b8e359c1599
iaguinzz <iagosilva7contato@gmail.com>
2021-02-15 15:25:11 -0200

    move windows right after open implemented

--- server/windowsManager.py
+++ server/windowsManager.py
@@ -3,15 +3,19 @@ import time
 from win32api import GetSystemMetrics
 
 def getW():
-
     windows = gw.getAllWindows()
-    
     return windows
 
 def moveW(w):
     width = GetSystemMetrics(0)#int(input("X: "))
 
     w.move(-width, 0)
+
+def getWindowsName():
+    janelas = []
+    for i in getW():
+        janelas.append(i.title)
+    return janelas
 
 def moveAll():
     windows = getW()
@@ -21,10 +25,24 @@ def moveAll():
             pass
         else:
             moveW(window)
+    moveContinuos(windows)
 
+def moveContinuos(windows):
+    while True:
+        for i in getW():
+            if i not in windows and len(i.title)>1:
+                print(f"Nova janela detectada: {i.title} Movendo..")
+                moveW(i)
 
+            elif getW().count(i) > 1:
+                print(f"Nova janela detectada: {i.title} Movendo..")
+                moveW(i)
+                
+        windows = getW()
+        time.sleep(.7)
+        
 
 if __name__ == '__main__':
 
-    moveAll()
+    moveContinuos(getW())
     
